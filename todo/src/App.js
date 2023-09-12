4092fc502899a9aec26852106789f47a1b40c071
Asnoyon <blackpain1100@gmail.com>
2023-09-12 20:09:40 +0600

    App js update

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-// import TodoList from './component/TodoList'
-import TodoForm from './component/TodoForm'
+import TodoList from './component/TodoList'
+// import TodoForm from './component/TodoForm'
 
 const App = () => {
   return (
     <>
-      {/* <TodoList/> */}
-      <TodoForm/>
+      <TodoList/>
+      {/* <TodoForm/> */}
     </>
   )
 }
